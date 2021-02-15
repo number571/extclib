4639c6f6d762d82f3f1f1ff983fb09ec68a1a2c4
number571 <number571@email.com>
2021-02-15 07:11:04 -0500

    add func.entropy

--- scrypto.c
+++ scrypto.c
@@ -1,10 +1,22 @@
+#ifdef __unix__
+    #include <stdio.h>
+#elif _WIN32
+    #include <windows.h>
+    #include <wincrypt.h>
+#else
+    #warning scrypto.h: used insecure entropy generator
+    #include <stdlib.h>
+    #include <time.h>
+#endif
+
 #include <stdint.h>
 
 static void swap(uint8_t *x, uint8_t *y);
 
 static uint8_t Sbox[256];
 
-extern void scrypto_rc4_init(char *key, int ksize) {
+// RC4
+extern void scrypto_srand(const char *key, int ksize) {
 	uint8_t j = 0;
 	for (int i = 0; i < 256; ++i) {
 		Sbox[i] = i;
@@ -15,7 +27,8 @@ extern void scrypto_rc4_init(char *key, int ksize) {
 	}
 }
 
-extern void scrypto_rc4_generate(char *output, int size) {
+// RC4
+extern void scrypto_rand(char *output, int size) {
 	uint8_t i = 0, j = 0;
 	uint8_t t;
 	for (int k = 0; k < size; ++k) {
@@ -27,10 +40,35 @@ extern void scrypto_rc4_generate(char *output, int size) {
 	}
 }
 
-extern void scrypto_xor(char *output, char *key, int ksize, char *input, int size) {
+extern void scrypto_xor(char *output, const char *key, int ksize, const char *input, int size) {
 	for (int i = 0; i < size; ++i) {
 		output[i] = input[i] ^ key[i % ksize];
 	}
+}
+
+extern int scrypto_entropy(char *output, int size) {
+#ifdef __unix__
+    FILE *file = fopen("/dev/random", "rb");
+    if (file == NULL) {
+        return 1;
+    }
+    fread(output, sizeof(char), size, file);
+    fclose(file);
+#elif _WIN32
+    HCRYPTPROV hCryptProv;
+    CryptAcquireContext(&hCryptProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
+    BOOL result = CryptGenRandom(hCryptProv, sizeof(char) * size, output);
+    if (result == 0) {
+        return 1;
+    }
+    CryptReleaseContext(hCryptProv, 0);
+#else
+    srand(time(NULL));
+    for (size_t i = 0; i < size; ++i) {
+        output[i] = rand() % UINT8_MAX;
+    }
+#endif
+    return 0;
 }
 
 static void swap(uint8_t *x, uint8_t *y) {
--- scrypto.h
+++ scrypto.h
@@ -1,9 +1,10 @@
 #ifndef EXTCLIB_SCRYPTO_H_
 #define EXTCLIB_SCRYPTO_H_
 
-extern void scrypto_rc4_init(char *key, int ksize);
-extern void scrypto_rc4_generate(char *output, int size);
+extern void scrypto_srand(const char *key, int ksize);
+extern void scrypto_rand(char *output, int size);
 
-extern void scrypto_xor(char *output, char *key, int ksize, char *input, int size);
+extern int scrypto_entropy(char *output, int size);
+extern void scrypto_xor(char *output, const char *key, int ksize, const char *input, int size);
 
 #endif /* EXTCLIB_SCRYPTO_H_ */
